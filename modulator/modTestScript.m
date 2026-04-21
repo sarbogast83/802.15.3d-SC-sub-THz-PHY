e34677a8d375fd1fbe0b200653ee249946279a8a
sarbogast83 <sarbogast83@gmail.com>
2026-04-21 16:33:20 -0400

    modulator complete matching theo BER

--- modulator/modTestScript.m
+++ modulator/modTestScript.m
@@ -1,29 +1,55 @@
-% pi/2 BPSK adn QPSK
-% % per 802.15.3d IEEE Computer Society, IEEE standard for wireless multimedia networks,
-% Revision of IEEE Std 802.15.3-2016, New York, NY, USA: IEEE LAN/MAN
-% Standards Committee, Nov. 2023. DOI: 10.1109/IEEESTD.2023.10332822.
 
 
+clear
+%%
+numBits = 1e6;
+bits = randi([0 1],numBits,1);
+EbNodB = -5:15;
 
 
-clear; close all
-%% init
-numBits = 1000;
-data = randi([0 1],numBits,1);
-M = 4; % BPSK=2, QPSK=4
+%% modulate
+% BPSK
+    bpsksyms = bits * 2 - 1; % mod 
+    n = (0:numBits-1)';
+    bpsksymsRotated = bpsksyms.*exp(1j*n*pi/2); % rotate
+  
+% QPSK   
+    qpskSyms = pskmod(bits,4,-pi/2,"gray","InputType","bit"); % note -pi/e rotation for standard
+    n = (0:numBits/2-1)';
+    qpskSymsRotated =  qpskSyms.*exp(1j*n*pi/2); 
 
 
-%% modulator
-switch M
-    case 2
+%% noise
+ebnoLin = 10.^(EbNodB/10);
+realNoise = randn(numBits,1);
+imagNoise = randn(numBits,1);
+noiseVec = realNoise + 1j*imagNoise;
+noiseScaler = 1./sqrt(2*ebnoLin);
+ber_theoretical = 0.5 * erfc(sqrt(ebnoLin));
+bpskBER = zeros(1,length(EbNodB));
+qpskBER = zeros(1,length(EbNodB));
+for i = 1:length(ebnoLin)
+   scaledNoiseVec = noiseScaler(i)*noiseVec;
+   noisybpsksymsRotated = bpsksymsRotated + scaledNoiseVec;
+   noisyqpskSymsRotated = qpskSymsRotated + scaledNoiseVec(1:numBits/2)/sqrt(2);
+    %% demod
+    % BPSK
         n = (0:numBits-1)';
-        syms = (data*2-1).*exp(1j * pi/2*n); 
-
-    case 4
-        bitPairs = reshape(data,2,[]);
-        
-
-    otherwise
-        disp('ERROR: Incorrect order M')
-        return
+        bpsksymsDeRotate = noisybpsksymsRotated.*exp(-1j*n*pi/2); % derotate
+        receivedBPSKbits = real(bpsksymsDeRotate) > 0; % logic
+        bpskBER(i) = sum(receivedBPSKbits~=bits)/numBits; 
+    % QPSK
+        n = (0:numBits/2-1)';
+        qpskSymsDeRotate =  noisyqpskSymsRotated.*exp(-1j*n*pi/2);
+        receivedQPSKbits = pskdemod(qpskSymsDeRotate,4,-pi/2,"gray",outputType='bit');
+        qpskBER(i) = sum(receivedQPSKbits~=bits)/numBits;
+    
+    
 end
+figure
+semilogy(EbNodB,ber_theoretical,EbNodB,bpskBER,'-x',EbNodB,qpskBER,'-o')
+legend('Theory','BPSK', 'QPSK')
+grid on 
+title('BER vs EbNo')
+xlabel('EbNo [dB]')
+ylabel('BER')
