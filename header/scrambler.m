0b1db64e13c4041d09838587f61111d4db205802
arbogasts <arbogasts>
2026-04-27 13:07:46 -0400

    commeted out self test

--- header/scrambler.m
+++ header/scrambler.m
@@ -13,7 +13,7 @@ scrambledBits = scrambleBits(testData,scramblerSeedID);
 function scrambledBits = scrambleBits(dataBits, scramblerSeedID)
     dataBits = dataBits(:); % work on columns
     scramblerSeedID = scramblerSeedID(:);
-    testVec =  [0; 0; 0; 1; 1; 1; 1; 0; 0; 0; 1; 1; 1; 0; 1; 0];
+%     testVec =  [0; 0; 0; 1; 1; 1; 1; 0; 0; 0; 1; 1; 1; 0; 1; 0];
     
     % [1 1 0 1 0 0 0 0 1 0 1 S1 S2 S3 S4]
     xInit = [1; 1; 0; 1; 0; 0; 0; 0; 1; 0; 1; scramblerSeedID]; %12-13
@@ -22,11 +22,11 @@ function scrambledBits = scrambleBits(dataBits, scramblerSeedID)
     for n = 1:16
 %     for n = 1:length(dataBits)
         xn = xor(xInit(14), xInit(15)); % 12-12
-        xnTest(n) = xn; 
+%         xnTest(n) = xn; 
         scrambledBits(n) = xor(dataBits(n), xn); % 12-14
         xInit = [xn; xInit(1:14)]; % shift
     end
-    if sum(xnTest ~= testVec) ~= 0
-        warning('scramble fail')
-    end
+%     if sum(xnTest ~= testVec) ~= 0
+%         warning('scramble fail')
+%     end
 end
