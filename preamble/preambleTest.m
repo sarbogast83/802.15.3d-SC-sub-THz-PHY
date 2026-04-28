4e38042239256fd1fb97784edcce13cb10478345
arbogasts <arbogasts>
2026-04-28 09:01:53 -0400

    SFD complete

--- preamble/preambleTest.m
+++ preamble/preambleTest.m
@@ -3,22 +3,23 @@
 %   Figure 13-5 shows the structure of the PHY-long or PHY-short preambles
 
 clear; close all
+load('golay128.mat')
+
 %% The SYNC field for
 % PHY-short preamble shall consist of 14 code repetitions of a128
 
-[Ga128,Gb128] = wlanGolaySequence(128); % these match the 802.15 
-Ga128 = [0 1 0 1 1 0 1 0 0 1 0 1 0 1 0 1 1 0 0 1 1 0 0 1 1 0 0 1 0 1 1 0 ...
-    0 0 1 1 1 1 0 0 0 0 1 1 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 ...
-    0 0 0 1 1 1 1 0 0 0 0 0 0 0 0 1 1 0 0 1 1 0 0 1 1 0 0 0 0 1 1 0 1 ...
-    1 0 1 0 0 1 0 1 1 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 1 0 1];
+SYNC = repmat(Ga,14,1); % column
 
+%% SFD
+% 12.2.4.2.3 SFD field
+% The SFD is used to establish frame timing as well as the header rate, either MR or HR. The SFD for the two
+% header rates are as follows:
+% — The MR header shall use an SFD with [+1 –1 +1 –1] spread by a128
+% — The HR header shall use an SFD with [+1 +1 –1 –1] spread by a128 *** high rate for data ***
 
-sync = repmat(Ga128,14,1); % column
-
-%% The SFD field shall consist of the sign inversion sequence
-%of a128
-
-SFD = Ga128 * -1; 
+HR = [1 1 -1 -1];
+HRGa = HR' * Ga';
+SFD = reshape(HRGa',[],1);
 
 %% CES 
 % The CES field, used for channel estimation, shall consist of [a256 b512 a512 b128] where the right most
